115757448fceefde893318a484576d84a7e18506
Graham Ollis <perl@wdlabs.com>
2013-05-08 13:37:12 -0400

    little bits

--- lib/PlatypusMan.pm
+++ lib/PlatypusMan.pm
@@ -3,7 +3,34 @@ package PlatypusMan;
 use strict;
 use warnings;
 use v5.10;
+use Path::Class::File;
+use Path::Class::Dir;
+use File::ShareDir qw( dist_dir );
 
+# ABSTRACT: Template based S5 Generation
 # VERSION
+
+sub share_dir
+{
+  state $path;
+  
+  unless(defined $path)
+  {
+    if(defined $PlatypusMan::VERSION)
+    {
+      $path = Path::Class::Dir
+        ->new(dist_dir('PlatypusMan'));
+    }
+    else
+    {
+      $path = Path::Class::File
+        ->new($INC{'PlatypusMan.pm'})
+        ->absolute
+        ->dir
+        ->parent
+        ->subdir('share');
+    }
+  }
+}
 
 1;
