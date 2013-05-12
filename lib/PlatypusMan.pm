ba7eb61d7650fb1c66596cda770f7fdc4fe1d00e
Graham Ollis <plicease@cpan.org>
2013-05-11 23:08:42 -0400

    sh hlighting

--- lib/PlatypusMan.pm
+++ lib/PlatypusMan.pm
@@ -10,6 +10,16 @@ use File::ShareDir qw( dist_dir );
 # ABSTRACT: Template based S5 Generation
 # VERSION
 
+=head1 BUNDLED FILES
+
+=head2 s5
+
+=head2 sh
+
+=head2 themes
+
+=cut
+
 sub share_dir
 {
   state $path;
